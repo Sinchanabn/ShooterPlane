04a5a4a2f46a01215fc8d7bd8e323b03ab40300a
TELXSI\sinchana.bn <sincana.bn@tataelxsi.co.in>
2021-11-25 20:37:40 +0530

    file added

---
+++ MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //This method will trigger when we click the play button
+   public void OnClickingPlay()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    //This method will trigger when we click the Quit button
+    public void OnClickingQuit()
+    {
+        Debug.Log("You are out of the game");
+        Application.Quit();
+    }
+}
